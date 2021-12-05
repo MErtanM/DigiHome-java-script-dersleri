a13973fbb97ca67166ad52e4e008b2859e614e21
MErtanM <mertan1971@gmail.com>
2021-12-05 22:20:49 +0300

    degiskenler added

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("hello World")
+
+var userName = "Ertan"
+console.log(userName)
+
+
+console.log(userName)
+userName = "Mertan"
+console.log(userName)
+let mail = "abc@mail.com" //* let ile atanilan veri degistirilebilir
+console.log(mail)
+mail = "xyz@gmail.com"
+console.log(mail)
+const email = "ababab" //* atanan veri degistirilemez parantez icinde ek islemler yapilabilir
+email = "hkakakakk"
+
+
+let uName;
+
+const secondName;
