cb8f4efb56a2e8307ba83a9054a75abf556bc6e8
MErtanM <mertan1971@gmail.com>
2021-12-06 08:10:15 +0300

    no commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+/*
 console.log("hello World")
-
+console.log("1", userName)
 var userName = "Ertan"
 console.log(userName)
 
@@ -17,4 +18,58 @@ email = "hkakakakk"
 
 let uName;
 
-const secondName;
+
+console.log(uName)
+let uName="Ertan"
+
+let firstUserName
+let userName
+let _name;
+let $name
+
+let 1name;//* kullanilamamz verilerinizi sadece makinenin anlamasi degil sonraki kisiler de anlayabilmeli
+
+let default;
+
+let a= 3;
+let b = 8;
+let c = a
+
+a=b;
+b=a;
+console.log(a, b)
+*/
+
+
+
+//-------DATA TYPES-----------------//
+/* 1.string;
+    2. number;
+    3.boolean;
+    4.undefined
+    5.null
+*/
+
+
+
+
+let name = "Ertan"
+let phoneNumber ;
+let email = null;
+let variable = `ali29`
+console.log(typeof varieble)
+
+
+console.log(phoneNumber)
+console.log(email)
+
+
+console.log(typeof name)
+
+console.log(typeof 29)
+console.log(typeof "125")
+console.log(typeof "true")
+console.log(typeof 3.14)
+
+console.log(typeof true)
+console.log(typeof false)
