fde7042ec396c5d5def06107f0e3b937004773b6
MErtanM <mertan1971@gmail.com>
2021-12-06 22:50:48 +0300

    i learn js

---
+++ basicoperators.js
@@ -0,0 +1,56 @@
+let currentYear =2021;
+let age = currentYear - 1971;
+console.log("age: ", age)
+
+let a = 3;
+let b = 8;
+
+let total = a + b;
+let totalValue = 5+8;
+console.log(total, totalValue)
+
+let x = 3*5;
+console.log(x)
+
+let y = 8/4;
+console.log(y)
+
+let exponent = 2**3;
+console.log(exponent)
+
+let mod = 10 / 3 ;
+console.log(mod)
+
+
+
+// increment, decrement bir artirma ve bir azaltma islemi yapar//
+// `--` 1 azaltir
+// `++` 1 artirir
+
+
+let ab = 3;
+let xy = --ab; // bu satirda ab =2 // azalt sonra ata
+console.log(ab, xy)   // bu satirda ab = 2
+
+
+let abc = 3;
+let xyz = abc-- // bu satirda abc = 3 //ilk once ata sonra azalt
+console.log(xyz, abc)
+
+
+// decrement artirma
+
+let dec = 5;
+let assg = ++dec;
+console.log(dec, assg)
+
+
+let dec2 = 5;
+let assg2 = dec2++
+console.log(assg2, dec2)
+
+//string consetanation   string ile birlestirme
+
+const firstName = "Jeff";
+const surName = "Bezos";
+console.log(firstName + " " +surName)
