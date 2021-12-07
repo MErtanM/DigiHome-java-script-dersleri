20439951fe7f93bdc5701abf2ad71c41006130f9
MErtanM <mertan1971@gmail.com>
2021-12-07 21:18:47 +0300

    no commit

--- basicoperators.js
+++ basicoperators.js
@@ -8,9 +8,9 @@ let b = 8;
 let total = a + b;
 let totalValue = 5+8;
 console.log(total, totalValue)
-
+/*
 let x = 3*5;
-console.log(x)
+console.log(x)*/
 
 let y = 8/4;
 console.log(y)
@@ -50,7 +50,62 @@ let assg2 = dec2++
 console.log(assg2, dec2)
 
 //string consetanation   string ile birlestirme
-
+/*
 const firstName = "Jeff";
 const surName = "Bezos";
 console.log(firstName + " " +surName)
+*/
+
+//Asssigment Operator
+/*
+let val = true;
+let x = 2
+
+x = x + 5
+console.log("x = " + x)
+
+x += 5
+console.log("x = " +x)
+
+let g= 16;
+g /= 2;
+console.log("g =" + g)
+
+ g *=4;
+console.log("g ="+ g)
+
+// x = x+ g
+
+x +=g
+console.log("x", x)
+*/
+
+//Comparison Operators
+
+/*< kucuk
+> buyuk
+<= kucuk esit 
+>= buyukesit */
+
+let ageAhmet =50;
+let ageUtku =28; // this gives boolen value dogru/yanlis
+console.log(ageAhmet > ageUtku)
+console.log(ageAhmet < ageUtku)
+console.log(ageAhmet == 50) // == anlami esit mi?
+console.log(ageUtku != 28) // != degil mi
+console.log("1", ageAhmet =="50" ) // yzi  da gelse number da gelse bizim icin ayni
+console.log("2", ageAhmet ==="50" ) // dta type olarak stringini kontrol ediyoruz
+console.log("2", ageAhmet !=="50" ) // data type 50 ye esit degil mi degil
+
+
+// Template Literal
+
+let frstName = "Jeff"
+let scnName = "Bezos"
+let work = "Amazon"
+
+let sentence = " He is " + frstName + "  " + scnName + " and He is faunder of" + work + ". ";
+console.log(sentence)
+
+let betterSentence = `He is ${frstName} ${scnName}, and He is founder of ${work}.`
+console.log(betterSentence)
